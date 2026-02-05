2df57d4fd2ce6730e9eb5292f215add06f4adba8
corymcguinness <corymcguinness@gmail.com>
2026-02-05 13:28:55 -0800

    Update page.tsx

--- app/[metro]/post/page.tsx
+++ app/[metro]/post/page.tsx
@@ -1,5 +1,11 @@
 import PostJobClient from "./post-client";
 
+export const dynamic = "force-static";
+
+export function generateStaticParams() {
+  return [{ metro: "portland-or" }];
+}
+
 export default async function Page({
   params
 }: {
